789b33ea0b3e41467be1ed44c89243ff8c5be309
David Verbenyi <dverbenyi@gmail.com>
2019-11-07 18:09:50 +0000

    Initial Commit

---
+++ Virus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Virus
+//
+//  Created by verebes on 07/11/2019.
+//  Copyright © 2019 David V. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Virus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Virus
+//
+//  Created by verebes on 07/11/2019.
+//  Copyright © 2019 David V. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
